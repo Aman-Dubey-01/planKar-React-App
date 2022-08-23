c6761bef2bcdc69daa9e98fc40681548c4ba432d
Aman Dubey <dubeyaman905@gmail.com>
2022-08-23 09:34:38 +0530

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+// import HighlightIcon from '@mui/icons-material/Highlight';
+import WorkHistoryIcon from '@mui/icons-material/WorkHistory';
+
+function Header() {
+  return (
+    <header>
+      <h1>
+        {/* <HighlightIcon /> */}
+        <WorkHistoryIcon />
+        &nbsp;𝔭𝔩𝔞𝔫𝓚𝔞𝔯
+        {/* <i>plan</i><b>K</b>ar */}
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
